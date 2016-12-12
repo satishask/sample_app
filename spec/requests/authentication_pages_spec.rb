3c7cdbe6dac0af14cf5d76518227cc7decf2a5e9
satish <satisha@freshdesk.com>
2016-12-12 22:14:57 +0530

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -22,7 +22,6 @@ describe "AuthenticationPages" do
 		describe "signin with valid information" do
 			let(:user) {FactoryGirl.create(:user)}
 			before do
-				user.save
 				fill_in "Email", with: user.email.upcase
 				fill_in "Password", with: user.password
 				click_button "Sign in"
@@ -32,6 +31,11 @@ describe "AuthenticationPages" do
 			it {should have_link('Profile', href: user_path(user))}
 			it {should have_link('Sign out', href: signout_path)}
 			it {should_not have_link('Sign in', href: signin_path)}
+
+			describe "followed by signout" do
+				before { click_link "Sign out" }
+				it { should have_link('Sign in') }
+			end
 		end
 	end
 end
