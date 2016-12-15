1e153001348aad6dac11f7b85b5139554d8324f0
satish <satisha@freshdesk.com>
2016-12-15 15:19:52 +0530

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
@@ -25,6 +26,9 @@ class User < ActiveRecord::Base
 
   after_validation { self.errors.messages.delete(:password_digest) }
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
     def create_remember_token
